e456fa04d8d8ed0796e3b7a0ffeed1e75238d196
Max Tolkoff <mtolkoff@gmail.com>
2019-05-17 11:04:46 -0400

    adds full hierarchical model, and outputs results to file

--- Bayesian_Model_Scores.py
+++ Bayesian_Model_Scores.py
@@ -23,14 +23,25 @@ if __name__ == '__main__':
 
     with pm.Model() as hierarchical_model:
         #Reading
-        intercept_reading = pm.Normal('intercept_reading', 0, sd=100)
-        lunch_reading_beta = pm.Normal('lunch_reading_beta', 0, sd=100)
-        gender_reading_beta = pm.Normal('gender_reading_beta', 0, sd=100)
-        testPrep_reading_beta = pm.Normal('testPrep_reading_beta', 0, sd=100)
-        race_reading_beta = pm.Normal('race_reading_beta', 0, sd=100, shape=(race_OneHot.shape[1]))
-        PEd_reading_beta = pm.Normal('PEd_reading_beta', 0, sd=100, shape=(PEd_OneHot.shape[1]))
+        intercept_mean = pm.Normal('intercept', 0, sd=100)
+        lunch_mean = pm.Normal('lunch_mean', 0, sd=100)
+        gender_mean = pm.Normal('gender_mean', 0, sd=100)
+        testPrep_mean = pm.Normal('testPrep_mean', 0, sd=100)
+        race_mean = pm.Normal('race_mean', 0, sd=100, shape=race_OneHot.shape[1])
+        PEd_mean = pm.Normal('PEd_mean', 0, sd=100, shape=PEd_OneHot.shape[1])
 
-        eps = pm.HalfCauchy('eps', 5)
+        global_shrinkage_model = pm.HalfCauchy('global_shrinkage', 5)
+        local_shrinkage_model = pm.HalfCauchy('local_shrinkage', 5, shape=3)
+
+        global_shrinkage_beta = pm.HalfCauchy('global_shrinkage_beta', 5)
+        local_shrinkage_beta = pm.HalfCauchy('local_shrinkage_beta', 5, shape=39)
+
+        intercept_reading = pm.Normal('intercept_reading', intercept_mean, sd=global_shrinkage_beta * local_shrinkage_beta[0])
+        lunch_reading_beta = pm.Normal('lunch_reading_beta', lunch_mean, sd=global_shrinkage_beta * local_shrinkage_beta[1])
+        gender_reading_beta = pm.Normal('gender_reading_beta', gender_mean, sd=global_shrinkage_beta * local_shrinkage_beta[2])
+        testPrep_reading_beta = pm.Normal('testPrep_reading_beta', testPrep_mean, sd=global_shrinkage_beta * local_shrinkage_beta[3])
+        race_reading_beta = pm.Normal('race_reading_beta', race_mean, sd=global_shrinkage_beta * local_shrinkage_beta[4:8], shape=(race_OneHot.shape[1]))
+        PEd_reading_beta = pm.Normal('PEd_reading_beta', PEd_mean, sd=global_shrinkage_beta * local_shrinkage_beta[8:13], shape=(PEd_OneHot.shape[1]))
 
         students_lm_reading = intercept_reading + lunch_reading_beta * lunchOneHot + gender_reading_beta * genderOneHot + \
                       testPrep_reading_beta * testPrepOneHot
@@ -39,15 +50,15 @@ if __name__ == '__main__':
         for i in range(PEd_OneHot.shape[1]):
             students_lm_reading = students_lm_reading + PEd_reading_beta[i] * PEd_OneHot.iloc[:, i]
 
-        reading_score = pm.Normal('reading', mu=students_lm_reading, sd=eps, observed=reading_transform)
+        reading_score = pm.Normal('reading', mu=students_lm_reading, sd=global_shrinkage_model * local_shrinkage_model[0], observed=reading_transform)
 
         #Writing
-        intercept_writing = pm.Normal('intercept_writing', 0, sd=100)
-        lunch_writing_beta = pm.Normal('lunch_writing_beta', 0, sd=100)
-        gender_writing_beta = pm.Normal('gender_writing_beta', 0, sd=100)
-        testPrep_writing_beta = pm.Normal('testPrep_writing_beta', 0, sd=100)
-        race_writing_beta = pm.Normal('race_writing_beta', 0, sd=100, shape=(race_OneHot.shape[1]))
-        PEd_writing_beta = pm.Normal('PEd_writing_beta', 0, sd=100, shape=(PEd_OneHot.shape[1]))
+        intercept_writing = pm.Normal('intercept_writing', intercept_mean, sd=global_shrinkage_beta * local_shrinkage_beta[13])
+        lunch_writing_beta = pm.Normal('lunch_writing_beta', lunch_mean, sd=global_shrinkage_beta * local_shrinkage_beta[14])
+        gender_writing_beta = pm.Normal('gender_writing_beta', gender_mean, sd=global_shrinkage_beta * local_shrinkage_beta[15])
+        testPrep_writing_beta = pm.Normal('testPrep_writing_beta', testPrep_mean, sd=global_shrinkage_beta * local_shrinkage_beta[16])
+        race_writing_beta = pm.Normal('race_writing_beta', race_mean, sd=global_shrinkage_beta * local_shrinkage_beta[17:21], shape=(race_OneHot.shape[1]))
+        PEd_writing_beta = pm.Normal('PEd_writing_beta', PEd_mean, sd=global_shrinkage_beta * local_shrinkage_beta[21:26], shape=(PEd_OneHot.shape[1]))
 
         students_lm_writing = intercept_writing + lunch_writing_beta * lunchOneHot + gender_writing_beta * genderOneHot + \
                       testPrep_writing_beta * testPrepOneHot
@@ -56,14 +67,14 @@ if __name__ == '__main__':
         for i in range(PEd_OneHot.shape[1]):
             students_lm_writing = students_lm_writing + PEd_writing_beta[i] * PEd_OneHot.iloc[:, i]
 
-        writing_score = pm.Normal('writing', mu=students_lm_writing, sd=eps, observed=writing_transform)
+        writing_score = pm.Normal('writing', mu=students_lm_writing, sd=global_shrinkage_model * local_shrinkage_model[1], observed=writing_transform)
 
-        intercept_math = pm.Normal('intercept_math', 0, sd=100)
-        lunch_math_beta = pm.Normal('lunch_math_beta', 0, sd=100)
-        gender_math_beta = pm.Normal('gender_math_beta', 0, sd=100)
-        testPrep_math_beta = pm.Normal('testPrep_math_beta', 0, sd=100)
-        race_math_beta = pm.Normal('race_math_beta', 0, sd=100, shape=(race_OneHot.shape[1]))
-        PEd_math_beta = pm.Normal('PEd_math_beta', 0, sd=100, shape=(PEd_OneHot.shape[1]))
+        intercept_math = pm.Normal('intercept_math', intercept_mean, sd=global_shrinkage_beta * local_shrinkage_beta[26])
+        lunch_math_beta = pm.Normal('lunch_math_beta', lunch_mean, sd=global_shrinkage_beta * local_shrinkage_beta[27])
+        gender_math_beta = pm.Normal('gender_math_beta', gender_mean, sd=global_shrinkage_beta * local_shrinkage_beta[28])
+        testPrep_math_beta = pm.Normal('testPrep_math_beta', testPrep_mean, sd=global_shrinkage_beta * local_shrinkage_beta[29])
+        race_math_beta = pm.Normal('race_math_beta', race_mean, sd=global_shrinkage_beta * local_shrinkage_beta[30:34], shape=(race_OneHot.shape[1]))
+        PEd_math_beta = pm.Normal('PEd_math_beta', PEd_mean, sd=global_shrinkage_beta * local_shrinkage_beta[34:39], shape=(PEd_OneHot.shape[1]))
 
         students_lm_math = intercept_math + lunch_math_beta * lunchOneHot + gender_math_beta * genderOneHot + \
                       testPrep_math_beta * testPrepOneHot
@@ -72,10 +83,13 @@ if __name__ == '__main__':
         for i in range(PEd_OneHot.shape[1]):
             students_lm_math = students_lm_math + PEd_math_beta[i] * PEd_OneHot.iloc[:, i]
 
-        math_score = pm.Normal('math_score', mu=students_lm_math, sd=eps, observed=math_transform)
+        math_score = pm.Normal('math_score', mu=students_lm_math, sd=global_shrinkage_model * local_shrinkage_model[2],
+                               observed=math_transform)
 
 
     with hierarchical_model:
-        hm_trace = pm.sample(500, tune=200)
+        pm.sampling.init_nuts(init='advi_map')
+        db = pm.backends.Text('testoutput.csv')
+        hm_trace = pm.sample(1000, tune=1000, trace=db)
     pm.traceplot(hm_trace)
     plt.show()
